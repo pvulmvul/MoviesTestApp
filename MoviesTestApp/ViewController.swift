fd68e7492e8faf421453646654e5fe57104d2c69
pvulmvul <pvulmvul@gmail.com>
2023-10-23 21:19:15 +0300

    Initial Commit

---
+++ MoviesTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesTestApp
+//
+//  Created by Paul Maul on 23.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
